8731e42ad12c2d8441479086db6938b75a6dfae3
AlexanderNap <nogaav@mail.ru>
2014-05-24 23:22:55 +0400

    Done  main script functionality.

--- cachematrix.R
+++ cachematrix.R
@@ -5,12 +5,32 @@
 ## Function for catching matrix data
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solve) m <<- solve
+    getInverse <- function() m
+    list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
+  }
 
-}
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    ## Return a matrix that is the inverse of 'x'
+    return (m)
 }
